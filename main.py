2f0def50a1e74192c745f4265a6047c1b05eb059
Joel <joelrequena@MacBook-Pro-de-Joel.local>
2026-07-02 18:33:40 +0200

    actualización readme + main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+# mi idea con la clase piloto es la siguiente:
+class Piloto:
+    def __init__(self, nombre, equipo, moto):
+        self.nombre = nombre
+        self.equipo = equipo
+        self.moto = moto
