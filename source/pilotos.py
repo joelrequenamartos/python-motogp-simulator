26491722e2296bea71fc3458925fe1f64c7c32a1
Joel <joelrequena@MacBook-Pro-de-Joel.local>
2026-07-04 19:27:40 +0200

    creacion campeonato y carreras individuales

---
+++ source/pilotos.py
@@ -0,0 +1,14 @@
+#Diccionario de pilotos.
+
+lista_pilotos = [
+    "Valentino Rossi", "Jorge Lorenzo", "Casey Stoner", "Max Biaggi",
+    "Sete Gibernau", "Loris Capirossi", "Max Verstappen", "Pecco Bagnaia", "Enea Bastianini",
+    "Fabio Quartararo", "Aleix Espargaró", "Jack Miller", "Johann Zarco", "Franco Morbidelli",
+    "Takaaki Nakagami", "Miguel Oliveira", "Luca Marini", "Marco Bezzecchi", "Augusto Fernández", "Alex Márquez"
+]
+
+lista_equipos = [
+    "Repsol Honda Team", "Yamaha Factory Racing", "Ducati Team", "Aprilia Racing", "KTM Factory Racing",
+    "Red Bull KTM Tech3", "LCR Honda", "Pramac Racing", "Gresini Racing", "Mooney VR46 Racing Team"
+]
+
